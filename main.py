e10d8d229aeeb9c7b2df94febfb35d3f37f68530
BlastedMeteor44 <65218875+ruhaan-bit@users.noreply.github.com>
2024-04-28 18:28:54 +0800

    added main

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+import string
+import pyperclip
+import time
+
+list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+
+def generate_random_string(length, characters):
+    return ''.join(random.choice(characters) for _ in range(length))
+
+random_string = generate_random_string(int(input("Enter length: ")), list)
+print("Copied to clipboard!")
+pyperclip.copy(random_string)
+time.sleep(0.5)
+print("Press Enter to exit...")
+input()
